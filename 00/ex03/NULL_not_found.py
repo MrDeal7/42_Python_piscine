6766a8e626ce9dcf2f249240f60f322b8fa8cef2
mr_deal <diogovalenteleal@gmail.com>
2026-03-13 22:21:59 +0000

    ex03 done

--- 00/ex03/NULL_not_found.py
+++ 00/ex03/NULL_not_found.py
@@ -1,2 +1,11 @@
 def NULL_not_found(object: any) -> int:
+	objType = type(object)
+	if (object == None) : print("Nothing:", object, objType)
+	elif (objType == float) : print("Cheese:", object, objType)
+	elif (objType == int) : print("Zero:", object, objType)
+	elif (object == "") : print("Empty:", object, objType)
+	elif (objType == bool) : print("Fake:", object, objType)
+	else : 
+		print("Type not Found:") 
+		return 1
 	return 0
